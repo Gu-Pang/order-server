861aad3d36a0a3bfb02422f675cb45e6dd6f51d8
kimdh32022 <kimdh32022@naver.com>
2026-03-31 01:05:23 +0900

    refactor: 중복된 패키지 스캔 설정 삭제
    - common에서 AppContext로 이미 설정된 값이기 때문에 삭제함.

--- src/main/java/org/gupang/order/OrderApplication.java
+++ src/main/java/org/gupang/order/OrderApplication.java
@@ -3,10 +3,7 @@ package org.gupang.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {
-		"org.gupang.common", // 공통 모듈 패키지명
-		"org.gupang.order"   // 현재 서비스 패키지명
-})
+@SpringBootApplication
 public class OrderApplication {
 
 	public static void main(String[] args) {
