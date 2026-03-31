8e3f28be1066b1096fc5232099d88c823435c9f2
kimdh32022 <kimdh32022@naver.com>
2026-03-31 18:46:46 +0900

    feat: 주문 상태값 추가
    - ORDER_SHPPING(배송 대기 중) 상태 값 추가

--- src/main/java/org/gupang/order/domain/Status.java
+++ src/main/java/org/gupang/order/domain/Status.java
@@ -4,6 +4,7 @@ public enum Status {
 
     ORDER_CREATING,      // 주문 생성 중
     ORDER_ACCEPT,        // 주문 접수
+    ORDER_SHPPING,       // 배송 대기 중
     ORDER_CANCEL,        // 주문 취소
     ORDER_COMPLETED,     // 주문 및 배송 완료
 
