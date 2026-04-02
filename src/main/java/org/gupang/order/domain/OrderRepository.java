4aca6a0570c7c91cb16e478556fa1b66741a7b80
kimdh32022 <kimdh32022@naver.com>
2026-04-02 20:23:08 +0900

    feat: 주문에 대한 상세 조회(1개)
    - 주문 번호 1개에 대한 주문 내역 조회 기능 추가

--- src/main/java/org/gupang/order/domain/OrderRepository.java
+++ src/main/java/org/gupang/order/domain/OrderRepository.java
@@ -2,7 +2,10 @@ package org.gupang.order.domain;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public interface OrderRepository {
     Order save(Order order);
+    Order findById(UUID orderId);
 }
