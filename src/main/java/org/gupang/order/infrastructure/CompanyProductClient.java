be35f1ec4d83211e5281123fc8c4f05cb62be31d
kimdh32022 <kimdh32022@naver.com>
2026-04-04 02:13:57 +0900

    feat: 외부 시스템 연동 및 인프라 서비스 구현
    - CompanyProductClient: 외부 서버 통신을 위한 Feign Client 구현
    - OrderProductServiceImpl: 외부 상품 정보를 도메인 모델로 변환하여 제공하는 기능 구현

--- src/main/java/org/gupang/order/infrastructure/CompanyProductClient.java
+++ src/main/java/org/gupang/order/infrastructure/CompanyProductClient.java
@@ -6,14 +6,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
 import java.util.UUID;
 
 @FeignClient(name = "company-server")
 public interface CompanyProductClient {
 
-    @GetMapping("/api/v1/companies/{company_id}")
-    CompanyResponseDto getCompany(@PathVariable("company_id") UUID companyId);
-
     @GetMapping("/api/v1/products/{product_id}")
-    ProductResponseDto getProduct(@PathVariable("product_id") UUID productId);
+    List<ProductResponseDto> getProducts(@PathVariable("product_id") List<UUID> productIds);
 }
+
--- src/main/java/org/gupang/order/infrastructure/OrderProductServiceImpl.java
+++ src/main/java/org/gupang/order/infrastructure/OrderProductServiceImpl.java
@@ -2,38 +2,23 @@ package org.gupang.order.infrastructure;
 
 import lombok.RequiredArgsConstructor;
 import org.gupang.order.domain.OrderProductService;
-import org.gupang.order.domain.dto.OrderCompanyInfo;
 import org.gupang.order.domain.dto.OrderProductInfo;
-import org.gupang.order.infrastructure.dto.CompanyResponseDto;
 import org.gupang.order.infrastructure.dto.ProductResponseDto;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class OrderProductServiceImpl implements OrderProductService {
+
     private final CompanyProductClient companyProductClient;
-    @Override
-    public OrderCompanyInfo getOrderCompanyInfo(UUID companyId) {
-        CompanyResponseDto companyResponseDto = companyProductClient.getCompany(companyId);
-        return new OrderCompanyInfo(
-                companyResponseDto.companyId(),
-                companyResponseDto.companyName(),
-                companyResponseDto.companyAddress(),
-                companyResponseDto.companyAddressDetail()
-        );
-    }
 
     @Override
-    public OrderProductInfo getOrderProductInfo(UUID productId) {
-        ProductResponseDto productResponseDto = companyProductClient.getProduct(productId);
+    public List<OrderProductInfo> getOrderProductInfo(List<UUID> productIds) {
+        List<ProductResponseDto> productResponseDtos = companyProductClient.getProducts(productIds);
 
-        return new OrderProductInfo(
-                productResponseDto.productId(),
-                productResponseDto.companyId(),
-                productResponseDto.stock(),
-                productResponseDto.price()
-        );
+        return productResponseDtos.stream().map(OrderProductInfo::from).toList();
     }
 }
