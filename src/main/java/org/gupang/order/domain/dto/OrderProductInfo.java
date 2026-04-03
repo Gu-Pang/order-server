646219bade8a7fc305cc921b9fd30d94f9029df8
kimdh32022 <kimdh32022@naver.com>
2026-04-04 02:22:13 +0900

    refactor: 주문 도메인 및 DTO 구조 개선
    - OrderProductInfo: ProductResponseDto를 도메인 모델로 변환하는 정적 팩토리 메서드 'from' 추가

--- src/main/java/org/gupang/order/domain/dto/OrderProductInfo.java
+++ src/main/java/org/gupang/order/domain/dto/OrderProductInfo.java
@@ -1,5 +1,7 @@
 package org.gupang.order.domain.dto;
 
+import org.gupang.order.infrastructure.dto.ProductResponseDto;
+
 import java.util.UUID;
 
 public record OrderProductInfo(
@@ -8,4 +10,12 @@ public record OrderProductInfo(
         Integer stock,
         Long price
 ) {
+    public static OrderProductInfo from(ProductResponseDto  productResponseDto) {
+        return new OrderProductInfo(
+                productResponseDto.productId(),
+                productResponseDto.companyId(),
+                productResponseDto.stock(),
+                productResponseDto.price()
+        );
+    }
 }
